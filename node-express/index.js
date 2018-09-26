58836ea05c4ead60ca5a22227cff5fed5a539c15
Aparus <rustamapaev@gmail.com>
2018-09-26 21:05:39 +0300

    Express Serve Static Files

--- node-express/index.js
+++ node-express/index.js
@@ -1,11 +1,14 @@
 const express = require("express"),
   http = require("http");
+const morgan = require("morgan");
 
 const hostname = "localhost";
 const port = 3000;
 
 const app = express();
 
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;

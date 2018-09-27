971516b1a516ee65681214f0622ac0c4886e80e4
Aparus <rustamapaev@gmail.com>
2018-09-27 22:05:35 +0300

    Mongoose Part 2

--- node-mongoose /index.js
+++ node-mongoose /index.js
@@ -8,17 +8,14 @@ const connect = mongoose.connect(url);
 connect.then(db => {
   console.log("Connected correctly to server");
 
-  var newDish = Dishes({
-    name: "Uthappizza",
-    description: "test"
-  });
-
-  newDish
-    .save()
+  Dishes.create({
+    name: "Uthapizza",
+    description: "Test"
+  })
     .then(dish => {
       console.log(dish);
 
-      return Dishes.find({});
+      return Dishes.find({}).exec();
     })
     .then(dishes => {
       console.log(dishes);

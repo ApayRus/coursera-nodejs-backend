a3a35f6e4950fb53ad5642bf3178de3ffa1bf9fc
Aparus <rustamapaev@gmail.com>
2018-09-29 22:28:36 +0300

    Cookies Авторизация делается не каждый раз, а через куки (basic + cookie)

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -25,29 +25,38 @@ connect.then(
 
 var app = express();
 
-// Basic authentication
+app.use(cookieParser("12345-67890-09876-54321"));
 
 function auth(req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
-    return;
-  }
-
-  var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == "admin" && pass == "password") {
-    next(); // authorized
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == "admin" && pass == "password") {
+      res.cookie("user", "admin", { signed: true });
+      next(); // authorized
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   } else {
-    var err = new Error("You are not authenticated!");
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    next(err);
+    if (req.signedCookies.user === "admin") {
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      next(err);
+    }
   }
 }
 

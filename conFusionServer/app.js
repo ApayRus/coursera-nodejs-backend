496c4bdcc631ba5ff0f0821348f3ad05165ea362
Aparus <rustamapaev@gmail.com>
2018-09-30 01:31:32 +0300

    Express Session Part 1 Авторизация через сессии, подобие куки,  сохраняются в файлах на сервере

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -25,10 +25,23 @@ connect.then(
 
 var app = express();
 
-app.use(cookieParser("12345-67890-09876-54321"));
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
+
+app.use(
+  session({
+    name: "session-id",
+    secret: "12345-67890-09876-54321",
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  })
+);
 
 function auth(req, res, next) {
-  if (!req.signedCookies.user) {
+  console.log(req.session);
+
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error("You are not authenticated!");
@@ -41,7 +54,7 @@ function auth(req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == "admin" && pass == "password") {
-      res.cookie("user", "admin", { signed: true });
+      req.session.user = "admin";
       next(); // authorized
     } else {
       var err = new Error("You are not authenticated!");
@@ -50,7 +63,8 @@ function auth(req, res, next) {
       next(err);
     }
   } else {
-    if (req.signedCookies.user === "admin") {
+    if (req.session.user === "admin") {
+      console.log("req.session: ", req.session);
       next();
     } else {
       var err = new Error("You are not authenticated!");

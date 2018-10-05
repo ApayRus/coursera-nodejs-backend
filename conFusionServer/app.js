428c2b3dd3ab1342afe6143c08f61ab827c2a965
Aparus <rustamapaev@gmail.com>
2018-10-05 12:44:51 +0300

    HTTPS создали сертификат, сервер и переадресацию с незащищенного на защищенный

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -32,6 +32,15 @@ connect.then(
 
 var app = express();
 
+// Secure traffic only
+app.all("*", (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(307, "https://" + req.hostname + ":" + app.get("secPort") + req.url);
+  }
+});
+
 app.use(
   session({
     name: "session-id",

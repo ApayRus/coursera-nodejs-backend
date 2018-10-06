26769322ca2f492b607cadc2506521791594f762
Aparus <rustamapaev@gmail.com>
2018-10-07 02:25:08 +0300

    File Upload ручка для загрузки картинок через Multer

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -18,6 +18,7 @@ var mongoose = require("mongoose");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 var config = require("./config");
+const uploadRouter = require("./routes/uploadRouter");
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -69,7 +70,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
-
+app.use("/imageUpload", uploadRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
